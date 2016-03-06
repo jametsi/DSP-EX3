626355b918438fddcaec2882e48099cd9a11f052
Jami Karvanen <jami@plop.fi>
2016-03-06 14:52:14 +0200

    Implement functionality for Step 1
    This replaces the form with a single input. When submitting, this
    input will be split into atomic operations that are executed on the
    backend in a sequence of synchronous requests.

--- client/client.js
+++ client/client.js
@@ -24,22 +24,38 @@ var client = {
         this.resultsElement.html('');
     },
     // submit form data and add solution to cache on success
-    submit: function() {
+    submitOperation: function(arg1, op, arg2) {
         var self = this;
-        var arg1 = $('.arg1').val();
-        var arg2 = $('.arg2').val();
-        var op = $('.op').val();
-
         $.ajax({
+            async: false,
             url: this.serverUrl,
             data: {
                 arg1: arg1,
-                arg2: arg2,
-                op: op
+                op: op,
+                arg2: arg2
             }
         }).done(function(data) {
             cache.add(data);
             self.printHistory();
         });
+    },
+    submit: function() {
+        var operation = $('.operation').val().split(' ');
+        if (operation.length < 3) {
+            alert('Invalid operation length. Separate the elements of the operation with space.')
+        }
+        var arg1 = operation[0];
+        var op = operation[1];
+        var arg2 = operation[2];
+
+        while (operation.length > 1) {
+            this.submitOperation(arg1, op, arg2);
+            operation = operation.slice(3);
+            operation.unshift(cache.history[0].split(' ')[4]);
+            arg1 = operation[0];
+            op = operation[1];
+            arg2 = operation[2];
+        }
+
     }
 };
