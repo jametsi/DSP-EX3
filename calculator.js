eea714eccda93a9a7c5baaa1532330cb1f2bc0ab
Jami Karvanen <jami@plop.fi>
2016-03-13 15:50:27 +0200

    Add pow operator (**) to server

--- calculator.js
+++ calculator.js
@@ -14,6 +14,9 @@ exports.calculate = function(num1, num2, op) {
         case "/":
             return num1 / num2;
             break;
+        case "**":
+            return Math.pow(num1, num2);
+            break;
         default:
             return "Error: Operation " + op + " not defined!"
     }
